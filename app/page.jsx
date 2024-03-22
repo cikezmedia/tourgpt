796630459ea6eee337a32071ecfa30561d6b62d3
Victor Chikezie <chikezievictorc@gmail.com>
2024-03-22 14:25:03 +0100

    Initial commit

---
+++ app/page.jsx
@@ -0,0 +1,76 @@
+import Link from "next/link";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <>
+      <section className="flex flex-col max-w-3xl text-center py-24 gap-5 px-4">
+        <h1 className="font-extrabold text-5xl leading-tight">
+          Do You Plan To Travel But Don't Know Where To Visit ?
+        </h1>
+        <p className="text-gray-400">
+          Tour GPT can help you explore countries and cities to know the best
+          city that matches your travel needs. It's super easy to plan your next
+          trip.
+        </p>
+        <div className="mt-3">
+          <Link
+            className="cursor-pointer capitalize font-medium px-8 py-3 bg-[#2B59FF] text-white rounded-3xl"
+            href="/"
+          >
+            Let's Get Started
+          </Link>
+        </div>
+      </section>
+      <section className="bg-gray-600 p-4 py-16 w-full mt-28">
+        <div className="flex flex-col-reverse gap-6 md:gap-0 md:flex-row mx-auto max-w-6xl items-center justify-between">
+          <div className="flex flex-col gap-5 md:w-1/2 w-full">
+            <h1 className="flex font-extrabold text-2xl md:text-3xl max-w-lg">
+              We have helped so many people plan their Travels in the past
+            </h1>
+            <div className="flex flex-col gap-3">
+              <p>
+                Tour GPT goes beyond basic travel planning by leveraging natural
+                language processing to understand your specific needs and
+                preferences. It can provide real-time updates on weather
+                conditions, local events, transportation options, and safety
+                advisories, ensuring a smooth and enjoyable journey from start
+                to finish.
+              </p>
+              <p>
+                Whether you're looking for off-the-beaten-path experiences,
+                iconic landmarks, culinary delights, or relaxation spots, Tour
+                GPT offers curated suggestions that align with your travel
+                goals.
+              </p>
+            </div>
+            <div className="flex items-center justify-between">
+              <div className="flex items-center gap-2">
+                <h1 className="font-extrabold text-4xl">120k+</h1>
+                <span className="text-green-300">Tours</span>
+              </div>
+              <div className="flex items-center gap-2">
+                <h1 className="font-extrabold text-4xl">3k+</h1>
+                <span className="text-green-300">Active Users</span>
+              </div>
+            </div>
+          </div>
+          <div className="md:w-1/2 w-full">
+            <Image
+              src="/image.png"
+              width={625}
+              height={460}
+              alt="Plan"
+              priority
+            />
+          </div>
+        </div>
+      </section>
+      <footer className="flex flex-col w-full items-center px-2 py-6 bg-[#12141d]">
+        <p className="text-gray-400">
+          Copyright © 2024 Tour GPT - All Rights Reserved.
+        </p>
+      </footer>
+    </>
+  );
+}
---
+++ components/Navbar.jsx
@@ -0,0 +1,66 @@
+"use client";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+import { HiOutlineMenuAlt3 } from "react-icons/hi";
+import { CgClose } from "react-icons/cg";
+
+const Navbar = () => {
+  const [show, setShow] = useState(false);
+  return (
+    <>
+      <nav className="flex justify-between items-center mx-auto p-6 max-w-6xl">
+        <Link className="cursor-pointer" href="/">
+          <Image src="/logo.png" alt="Logo" width="130" height="48" />
+        </Link>
+        <ul className="hidden md:flex items-center space-x-8 font-medium">
+          <Link className="cursor-pointer" href="/chat">
+            Chat
+          </Link>
+          <Link className="cursor-pointer" href="/tours">
+            Tours
+          </Link>
+          <Link className="cursor-pointer" href="/profile">
+            Profile
+          </Link>
+          <Link
+            className="cursor-pointer font-medium text-sm capitalize px-8 py-3 bg-gray-700 text-white rounded-3xl"
+            href="/chat"
+          >
+            Let's Get Started
+          </Link>
+        </ul>
+        <div className="md:hidden">
+          {show ? (
+            <CgClose onClick={() => setShow(!show)} size={30} />
+          ) : (
+            <HiOutlineMenuAlt3 onClick={() => setShow(!show)} size={30} />
+          )}
+        </div>
+      </nav>
+      {show && (
+        <ul className="absolute md:hidden p-10 z-10 flex flex-col bg-[#2B59FF] w-full gap-6 font-medium">
+          <Link className="cursor-pointer text-lg" href="/chat">
+            Chat
+          </Link>
+          <Link className="cursor-pointer text-lg" href="/tours">
+            Tours
+          </Link>
+          <Link className="cursor-pointer text-lg" href="/profile">
+            Profile
+          </Link>
+          <div className="mx-auto mt-4">
+            <Link
+              className="cursor-pointer font-semibold text-sm capitalize px-8 py-3 bg-white text-gray-700 rounded-3xl"
+              href="/chat"
+            >
+              Let's Get Started
+            </Link>
+          </div>
+        </ul>
+      )}
+    </>
+  );
+};
+
+export default Navbar;
